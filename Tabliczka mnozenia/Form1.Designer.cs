ccf64630ef2f0f5312e0e6e603f706a49d4525c5
macio <macio@DESKTOP-JAP4NCB>
2017-06-29 12:21:52 +0200

    Add project files.

---
+++ Tabliczka mnozenia/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Tabliczka_mnozenia
+{
+    partial class Tabliczka_Mnozenia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.przyciskDalej = new System.Windows.Forms.Button();
+            this.tekstPoziom = new System.Windows.Forms.Label();
+            this.tekstCzas = new System.Windows.Forms.Label();
+            this.poziom = new System.Windows.Forms.Label();
+            this.mnoznik = new System.Windows.Forms.Label();
+            this.druga_cyfra = new System.Windows.Forms.Label();
+            this.pierwsza_cyfra = new System.Windows.Forms.Label();
+            this.czas = new System.Windows.Forms.Label();
+            this.wynik = new System.Windows.Forms.TextBox();
+            this.start = new System.Windows.Forms.Button();
+            this.wlasciwosci = new System.Windows.Forms.Button();
+            this.rownasie = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // przyciskDalej
+            // 
+            this.przyciskDalej.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.przyciskDalej.Location = new System.Drawing.Point(329, 100);
+            this.przyciskDalej.Name = "przyciskDalej";
+            this.przyciskDalej.Size = new System.Drawing.Size(76, 40);
+            this.przyciskDalej.TabIndex = 0;
+            this.przyciskDalej.Text = "Dalej";
+            this.przyciskDalej.UseVisualStyleBackColor = true;
+            // 
+            // tekstPoziom
+            // 
+            this.tekstPoziom.AutoSize = true;
+            this.tekstPoziom.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tekstPoziom.Location = new System.Drawing.Point(31, 23);
+            this.tekstPoziom.Name = "tekstPoziom";
+            this.tekstPoziom.Size = new System.Drawing.Size(151, 39);
+            this.tekstPoziom.TabIndex = 1;
+            this.tekstPoziom.Text = "Poziom:";
+            // 
+            // tekstCzas
+            // 
+            this.tekstCzas.AutoSize = true;
+            this.tekstCzas.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tekstCzas.Location = new System.Drawing.Point(317, 26);
+            this.tekstCzas.Name = "tekstCzas";
+            this.tekstCzas.Size = new System.Drawing.Size(104, 37);
+            this.tekstCzas.TabIndex = 2;
+            this.tekstCzas.Text = "Czas:";
+            // 
+            // poziom
+            // 
+            this.poziom.AutoSize = true;
+            this.poziom.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.poziom.Location = new System.Drawing.Point(169, 27);
+            this.poziom.Name = "poziom";
+            this.poziom.Size = new System.Drawing.Size(142, 37);
+            this.poziom.TabIndex = 3;
+            this.poziom.Text = "POZIOM";
+            this.poziom.UseMnemonic = false;
+            // 
+            // mnoznik
+            // 
+            this.mnoznik.AutoSize = true;
+            this.mnoznik.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.mnoznik.Location = new System.Drawing.Point(172, 103);
+            this.mnoznik.Name = "mnoznik";
+            this.mnoznik.Size = new System.Drawing.Size(30, 37);
+            this.mnoznik.TabIndex = 4;
+            this.mnoznik.Text = "*";
+            // 
+            // druga_cyfra
+            // 
+            this.druga_cyfra.AutoSize = true;
+            this.druga_cyfra.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.druga_cyfra.Location = new System.Drawing.Point(208, 103);
+            this.druga_cyfra.Name = "druga_cyfra";
+            this.druga_cyfra.Size = new System.Drawing.Size(39, 37);
+            this.druga_cyfra.TabIndex = 5;
+            this.druga_cyfra.Text = "Y";
+            // 
+            // pierwsza_cyfra
+            // 
+            this.pierwsza_cyfra.AutoSize = true;
+            this.pierwsza_cyfra.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.pierwsza_cyfra.Location = new System.Drawing.Point(128, 103);
+            this.pierwsza_cyfra.Name = "pierwsza_cyfra";
+            this.pierwsza_cyfra.Size = new System.Drawing.Size(38, 37);
+            this.pierwsza_cyfra.TabIndex = 6;
+            this.pierwsza_cyfra.Text = "X";
+            // 
+            // czas
+            // 
+            this.czas.AutoSize = true;
+            this.czas.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.czas.Location = new System.Drawing.Point(427, 27);
+            this.czas.Name = "czas";
+            this.czas.Size = new System.Drawing.Size(103, 37);
+            this.czas.TabIndex = 7;
+            this.czas.Text = "CZAS";
+            // 
+            // wynik
+            // 
+            this.wynik.Location = new System.Drawing.Point(283, 113);
+            this.wynik.MinimumSize = new System.Drawing.Size(4, 40);
+            this.wynik.Name = "wynik";
+            this.wynik.Size = new System.Drawing.Size(40, 40);
+            this.wynik.TabIndex = 8;
+            // 
+            // start
+            // 
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.start.Location = new System.Drawing.Point(135, 175);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(270, 45);
+            this.start.TabIndex = 9;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // wlasciwosci
+            // 
+            this.wlasciwosci.ForeColor = System.Drawing.Color.Maroon;
+            this.wlasciwosci.Location = new System.Drawing.Point(135, 146);
+            this.wlasciwosci.Name = "wlasciwosci";
+            this.wlasciwosci.Size = new System.Drawing.Size(270, 23);
+            this.wlasciwosci.TabIndex = 10;
+            this.wlasciwosci.Text = "Właściwości";
+            this.wlasciwosci.UseVisualStyleBackColor = true;
+            // 
+            // rownasie
+            // 
+            this.rownasie.AutoSize = true;
+            this.rownasie.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rownasie.Location = new System.Drawing.Point(240, 103);
+            this.rownasie.Name = "rownasie";
+            this.rownasie.Size = new System.Drawing.Size(37, 37);
+            this.rownasie.TabIndex = 11;
+            this.rownasie.Text = "=";
+            // 
+            // Tabliczka_Mnozenia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 274);
+            this.Controls.Add(this.rownasie);
+            this.Controls.Add(this.wlasciwosci);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.wynik);
+            this.Controls.Add(this.czas);
+            this.Controls.Add(this.pierwsza_cyfra);
+            this.Controls.Add(this.druga_cyfra);
+            this.Controls.Add(this.mnoznik);
+            this.Controls.Add(this.poziom);
+            this.Controls.Add(this.tekstCzas);
+            this.Controls.Add(this.tekstPoziom);
+            this.Controls.Add(this.przyciskDalej);
+            this.Name = "Tabliczka_Mnozenia";
+            this.Text = "Tabliczka Mnożenia";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button przyciskDalej;
+        private System.Windows.Forms.Label tekstPoziom;
+        private System.Windows.Forms.Label tekstCzas;
+        private System.Windows.Forms.Label poziom;
+        private System.Windows.Forms.Label mnoznik;
+        private System.Windows.Forms.Label druga_cyfra;
+        private System.Windows.Forms.Label pierwsza_cyfra;
+        private System.Windows.Forms.Label czas;
+        private System.Windows.Forms.TextBox wynik;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button wlasciwosci;
+        private System.Windows.Forms.Label rownasie;
+    }
+}
+
---
+++ Tabliczka mnozenia/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabliczka_mnozenia
+{
+    public partial class Tabliczka_Mnozenia : Form
+    {
+
+        public Tabliczka_Mnozenia()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            start.Visible = false;
+        }
+    }
+}
